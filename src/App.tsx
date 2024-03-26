294bbc0bca57955f4c4146d5ff5c968614c3b22b
huanc0de <arturfurtak5@gmail.com>
2024-03-26 21:40:57 +0100

    added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import styles from './App.module.scss';
 import Home from './pages/Home/Home';
-// import Register from './pages/Register/Register';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
     <div className={styles.container}>
+      <Register />
       <Home />
-      {/* <Register /> */}
     </div>
   );
 }
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,13 +1,41 @@
 import styles from './Register.module.scss';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import '../../Firebase';
 
 const Register = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log((e.currentTarget[0] as HTMLInputElement).value);
+    // const username = (e.currentTarget[0] as HTMLInputElement).value;
+    const email = (e.currentTarget[1] as HTMLInputElement).value;
+    const password = (e.currentTarget[2] as HTMLInputElement).value;
+    // const file = (e.currentTarget[4] as HTMLInputElement).value;
+
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        // ...
+        console.log(user.email);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ..
+      });
+  };
+
   return (
     <div className={styles.container}>
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <input type="text" placeholder="Username" />
         <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm password" />
+        <input type="new-password" placeholder="Password" />
+        <input type="new-password" placeholder="Confirm password" />
         <input type="file" />
         <button>Sign Up</button>
       </form>
