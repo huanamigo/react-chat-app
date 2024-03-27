d572808ea3d5ad1aa73028338386322c79e8e166
huanc0de <arturfurtak5@gmail.com>
2024-03-27 22:01:20 +0100

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import styles from './App.module.scss';
 import Home from './pages/Home/Home';
 import Register from './pages/Register/Register';
@@ -5,8 +6,10 @@ import Register from './pages/Register/Register';
 function App() {
   return (
     <div className={styles.container}>
-      <Register />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
