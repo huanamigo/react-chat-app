07707abec0186ed86af47d3a3e6e5c42be06430d
huanc0de <arturfurtak5@gmail.com>
2024-05-13 22:51:44 +0200

    added chat logic

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -4,24 +4,46 @@ import Message from './Message/Message';
 import MessageInput from './MessageInput/MessageInput';
 import { ChatContext } from '../../context/ChatContext';
 
+interface IUserFromContext {
+  user?: {
+    photoURL?: string;
+    displayName?: string;
+    uid?: string;
+  };
+}
+
 const Chat = () => {
-  const { dispatch }: models.IChatContext = useContext(ChatContext);
+  const { user }: IUserFromContext = useContext(ChatContext);
+
+  console.log(user);
 
   return (
     <div className={styles.container}>
-      <Message messageText="11111 1111 1111 1111111 1 111" time="15:40" />
-      <Message messageText="22222222222222222222" time="15:40" />
-      <Message
-        messageText="333333 3333333 3333333333 3333333 333"
-        time="15:40"
-      />
-      <Message messageText="444 44444 444 44444  44" time="15:40" />
-      <Message messageText="5555 5555555 5555 5555 555 5555" time="15:40" />
-      <Message
-        messageText="55555555555555555555555555555555555555"
-        time="15:40"
-      />
-      <MessageInput />
+      <div className={styles.topbar}>
+        {user ? (
+          <>
+            <img src={user.photoURL} alt="User photo" />
+            <p>{user.displayName}</p>
+          </>
+        ) : (
+          <p>USER NOT FOUND</p>
+        )}
+      </div>
+      <div className={styles.messageWrapper}>
+        <Message messageText="11111 1111 1111 1111111 1 111" time="15:40" />
+        <Message messageText="22222222222222222222" time="15:40" />
+        <Message
+          messageText="333333 3333333 3333333333 3333333 333"
+          time="15:40"
+        />
+        <Message messageText="444 44444 444 44444  44" time="15:40" />
+        <Message messageText="5555 5555555 5555 5555 555 5555" time="15:40" />
+        <Message
+          messageText="55555555555555555555555555555555555555"
+          time="15:40"
+        />
+        <MessageInput />
+      </div>
     </div>
   );
 };
